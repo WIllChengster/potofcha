704e8cde7c0bf609a954d3f07c0b68498f23bd85
burninggun <willchengsters@gmail.com>
2019-02-02 19:04:16 -0800

    starting home screeng

---
+++ src/components/hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const hero = () => (
+  <div style={{
+      backgroundColor: 'gray',
+      height: '250px',
+      margin: '-2rem -1.0875rem 0rem',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}
+  >
+    <h3>Welcome to pot of cha</h3>
+  </div>
+)
+
+export default hero
